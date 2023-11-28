61052c688651acd1b25fbe460d930fc2c94b2be6
andres-saa <andrew19f@gmail.com>
2023-11-27 23:43:06 -0500

    dist

---
+++ frontend/dist/assets/auto.esm-f873b0fc.js
@@ -0,0 +1,1 @@
+import{Z as r,$ as e}from"./index-058d29f9.js";r.register(...e);export{r as default};
---
+++ frontend/dist/assets/sedes-a9b9ae33.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-058d29f9.js";const r={};function c(n,s){return null}const _=e(r,[["render",c]]);export{_ as default};
