3a774d1525c009e8470220b0972f5343fcd663d2
andres-saa <andrew19f@gmail.com>
2023-11-28 22:19:07 -0500

    build

--- frontend/dist/assets/auto.esm-327fa525.js
+++
@@ -1,1 +0,0 @@
-import{$ as r,a0 as a}from"./index-f870463a.js";r.register(...a);export{r as default};
---
+++ frontend/dist/assets/auto.esm-f2bd1477.js
@@ -0,0 +1,1 @@
+import{$ as r,a0 as a}from"./index-4db8c5e6.js";r.register(...a);export{r as default};
