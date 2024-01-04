63a65695caa0b88887b74cd567f01523d875c37d
andres-saa <andres-saa>
2024-01-04 14:33:59 +0000

    correcciones

--- backend-app/web-env/bin/activate.fish
+++ backend-app/web-env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ludi/web_salchimonster/backend-app/web-env"
+set -gx VIRTUAL_ENV "/root/web_salchimonster/backend-app/web-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
