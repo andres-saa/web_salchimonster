898d53b56847b39268ee994492cd89824ee8c015
andres-saa <andrew19f@gmail.com>
2024-02-07 01:00:51 -0500

    correccion de error de las sedes

--- frontend/dist/assets/auto.esm-01755dcb.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a,a9 as r}from"./index-50803bd0.js";a.register(...r);export{a as default};
---
+++ frontend/dist/assets/auto.esm-940d3fd2.js
@@ -0,0 +1,1 @@
+import{a8 as a,a9 as r}from"./index-f55a7517.js";a.register(...r);export{a as default};
