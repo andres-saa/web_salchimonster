8f73c63c17267dafc3f0ba1aaddcd13f0ce0a9a4
andres-saa <andrew19f@gmail.com>
2024-02-14 10:53:20 -0500

    build

---
+++ frontend/dist/assets/auto.esm-5f837b0f.js
@@ -0,0 +1,1 @@
+import{a8 as a,a9 as r}from"./index-77eec66d.js";a.register(...r);export{a as default};
--- frontend/dist/assets/auto.esm-dcf4e4d6.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a,a9 as r}from"./index-16db287b.js";a.register(...r);export{a as default};
