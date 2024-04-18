4d2c6ebf280ff6daa5ae91a2b90b9fcc733faeca
andres-saa <andrew19f@gmail.com>
2024-04-18 11:20:09 -0500

    nuevo back

--- backend-app/models/adicionales_new.py
+++ backend-app/models/adicionales_new.py
@@ -133,52 +133,52 @@ class Adicional:
 
         return f"Updated {updated_rows} items named '{aditional_item_name}' with price {price} at site {site_id} to {'active' if status else 'inactive'}."
     
-    # def select_adicionales_for_product(self, product_instance_ids):
-    # # Convertir la lista de IDs en una cadena para la consulta SQL
-    #     ids_string = ', '.join(map(str, product_instance_ids))
+    def select_adicionales_for_product(self, product_instance_ids):
+    # Convertir la lista de IDs en una cadena para la consulta SQL
+        ids_string = ', '.join(map(str, product_instance_ids))
         
-    #     # Ejecutar la consulta para obtener los detalles adicionales de los productos
-    #     select_query = f"""
-    #     SELECT DISTINCT  aditional_item_name,aditional_item_instance_id, product_category_name, aditional_item_price, aditional_item_type_name
-    #     FROM inventory.product_aditional_details
-    #     WHERE product_instance_id IN ({ids_string});
-    #     """
-    #     self.cursor.execute(select_query)
+        # Ejecutar la consulta para obtener los detalles adicionales de los productos
+        select_query = f"""
+        SELECT DISTINCT  aditional_item_name,aditional_item_instance_id, product_category_name, aditional_item_price, aditional_item_type_name
+        FROM inventory.product_aditional_details
+        WHERE product_instance_id IN ({ids_string});
+        """
+        self.cursor.execute(select_query)
         
-    #     # Obtener los nombres de las columnas del resultado
-    #     columns = [desc[0] for desc in self.cursor.description]
+        # Obtener los nombres de las columnas del resultado
+        columns = [desc[0] for desc in self.cursor.description]
         
-    #     # Crear un conjunto para eliminar duplicados
-    #     unique_additionals = set()
-    #     additional_details = []
+        # Crear un conjunto para eliminar duplicados
+        unique_additionals = set()
+        additional_details = []
         
-    #     # Recopilar datos únicos
-    #     for row in self.cursor.fetchall():
-    #         # Crear un tuple, que es hashable y se puede añadir a un set
-    #         detail_tuple = tuple(row)
-    #         if detail_tuple not in unique_additionals:
-    #             unique_additionals.add(detail_tuple)
-    #             additional_details.append(dict(zip(columns, row)))
+        # Recopilar datos únicos
+        for row in self.cursor.fetchall():
+            # Crear un tuple, que es hashable y se puede añadir a un set
+            detail_tuple = tuple(row)
+            if detail_tuple not in unique_additionals:
+                unique_additionals.add(detail_tuple)
+                additional_details.append(dict(zip(columns, row)))
 
-    #     # Agrupar los detalles adicionales por tipo
-    #     grouped_details = {}
-    #     for detail in additional_details:
-    #         type_name = detail["aditional_item_type_name"]
-    #         if type_name not in grouped_details:
-    #             grouped_details[type_name] = [detail]
-    #         else:
-    #             grouped_details[type_name].append(detail)
+        # Agrupar los detalles adicionales por tipo
+        grouped_details = {}
+        for detail in additional_details:
+            type_name = detail["aditional_item_type_name"]
+            if type_name not in grouped_details:
+                grouped_details[type_name] = [detail]
+            else:
+                grouped_details[type_name].append(detail)
         
-    #     # Preparar la salida agrupada
-    #     grouped_output = []
-    #     for type_name, items in grouped_details.items():
-    #         category_items = {
-    #             "category": type_name,
-    #             "items": items
-    #         }
-    #         grouped_output.append(category_items)
+        # Preparar la salida agrupada
+        grouped_output = []
+        for type_name, items in grouped_details.items():
+            category_items = {
+                "category": type_name,
+                "items": items
+            }
+            grouped_output.append(category_items)
 
-    #     return grouped_output
+        return grouped_output
    
 
     def select_all_aditional_registered(self):
@@ -209,18 +209,7 @@ class Adicional:
         
         
         
-        
-       
-        
-    
-    def select_adicionales_for_product(self, product_instance_id):
-        select_query = f"SELECT * FROM inventory.product_aditional_details WHERE product_instance_id = {product_instance_id};"
-        self.cursor.execute(select_query)
-        columns = [desc[0] for desc in self.cursor.description]
-        return [dict(zip(columns, row)) for row in self.cursor.fetchall()]
-    
-        
-        
+     
         
     def close_connection(self):
         self.conn.close()
