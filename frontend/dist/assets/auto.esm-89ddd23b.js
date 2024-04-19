9be641ec371cfd99719b28b8272a3b96076b3771
andres-saa <andrew19f@gmail.com>
2024-04-19 16:29:06 -0500

    correccion ordenes prueba

--- frontend/dist/assets/auto.esm-89ddd23b.js
+++
@@ -1,1 +0,0 @@
-import{S as r,V as e}from"./index-36a1cf88.js";r.register(...e);export{r as default};
---
+++ frontend/dist/assets/auto.esm-f2f19464.js
@@ -0,0 +1,1 @@
+import{S as r,V as e}from"./index-49dfc7f3.js";r.register(...e);export{r as default};
