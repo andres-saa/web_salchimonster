ee451ee6b6a21d41d59be609550d2cae818170b8
andres-saa <andrew19f@gmail.com>
2024-04-23 09:22:58 -0500

    correccion de sonido y notificaciones

--- frontend-delivery/src/App.vue
+++ frontend-delivery/src/App.vue
@@ -1,94 +1,69 @@
 <script setup>
-import { URI_SOCKET } from '@/service/conection';
-import { onBeforeUnmount, onMounted , onUnmounted,ref} from 'vue';
+import { onBeforeUnmount, onMounted, onUnmounted, ref } from 'vue';
 import router from './router';
 import { useOrderStore } from './store/order';
 import { useSitesStore } from './store/site';
-const sitestore = useSitesStore()
-const store = useOrderStore();
 import { orderService } from './service/orderService';
-
-
+import { URI } from './service/conection';
+const sitestore = useSitesStore();
+const store = useOrderStore();
 
 const requestNotificationPermission = async () => {
     const permission = await Notification.requestPermission();
     if (permission !== 'granted') {
         alert('Las notificaciones están deshabilitadas. Por favor, habilite las notificaciones para obtener alertas en tiempo real.');
     }
-}
+};
+
+
+const playNotificationSound = () => {
+    if (store.Notification.readyState >= 2) { // Verifica que el audio esté suficientemente cargado para reproducirse
+        store.Notification.play().then(() => {
+            store.Notification.loop = true;
+        }).catch(error => {
+            console.error('Error playing sound:', error);
+        });
+    } else {
+        console.error('Sound not ready for playback');
+    }
+};
+
 
 onMounted(() => {
-  // Solicitar permiso de notificación al cargar el componente
-  requestNotificationPermission();
-
-  // Definir una función que realiza las acciones deseadas
-  async function fetchOrdersAndNotify() {
-    try {
-      const site_id = await sitestore.site.site_id; // Asumiendo que `sitestore` está correctamente importado/inicializado
-      const count_orders = await orderService.getOrderCount(site_id); // Asumir `orderService` está importado
-
-      if (store.currentCountOrders < count_orders) {
-        store.currentCountOrders = count_orders;
-        await store.getTodayOrders() // Asegúrate de actualizar el estado con el nuevo conteo
-        await store.Notification.play();
-        store.Notification.addEventListener('ended', function() {
-          this.currentTime = 0;
-          this.play();
-        }, false);
-      }
-    } catch (error) {
-      console.error('Error fetching orders:', error);
-    }
-  }
-
-
-
-  async function fetchOrdersAndNotify() {
-    try {
-      const site_id = await sitestore.site.site_id; // Asumiendo que `sitestore` está correctamente importado/inicializado
-      const order_response = await orderService.is_recent_order_generated(site_id); // Asumir `orderService` está importado
-
-      if (order_response &&  store.last_order_id !== order_response) {
-        store.last_order_id = order_response;
-        await store.getTodayOrders() // Asegúrate de actualizar el estado con el nuevo conteo
-        await store.Notification.play();
-        store.Notification.addEventListener('ended', function() {
-          this.currentTime = 0;
-          this.play();
-        }, false);
-      }
-    } catch (error) {
-      console.error('Error fetching orders:', error);
-    }
-  }
-
-  const intervalId = setInterval(fetchOrdersAndNotify, 3000);
-
-  // Limpieza del intervalo cuando el componente se desmonte
-  onUnmounted(() => {
-    clearInterval(intervalId);
-  });
+    requestNotificationPermission();
+    const fetchOrdersAndNotify = async () => {
+        try {
+            const site_id = sitestore.site.site_id; // Asumiendo que `sitestore` está correctamente importado/inicializado
+            const order_response = await orderService.is_recent_order_generated(site_id);
+            if (order_response && store.last_order_id !== order_response) {
+                store.last_order_id = order_response;
+                await store.getTodayOrders(); // Asegúrate de actualizar el estado con el nuevo conteo
+                playNotificationSound();
+                
+                if (Notification.permission === 'granted') {
+                    const notification = new Notification('Nueva Orden en la página de WhatsApp', {
+                        body: 'Nueva Orden en la página de WhatsApp',
+                        icon: '/images/logo.png',
+                        image: `${URI}/read-product-image/300/site-${site_id}`
+                    });
+                    notification.onclick = () => {
+                        window.focus();
+                    };
+                }
+            }
+        } catch (error) {
+            console.error('Error fetching orders:', error);
+        }
+    };
+    
+    const intervalId = setInterval(fetchOrdersAndNotify, 3000);
+    onUnmounted(() => {
+        clearInterval(intervalId);
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 </script>
 
 <template>
-
-  <router-view  class="col-12 p-0 " />
-
+    <router-view class="col-12 p-0" />
 </template>
-
