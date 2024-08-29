4dc1c4bc69de10db02ae50a61985f65d8f07a21b
andres-saa <andrew19f@gmail.com>
2024-08-29 11:11:20 -0500

    nuevas rutas

---
+++ frontend/dist/assets/auto.esm-0d706069.js
@@ -0,0 +1,1 @@
+import{W as r,X as e}from"./index-793ae813.js";r.register(...e);export{r as default};
--- frontend/dist/assets/auto.esm-64acb71d.js
+++
@@ -1,1 +0,0 @@
-import{W as r,X as e}from"./index-02dca8a6.js";r.register(...e);export{r as default};
