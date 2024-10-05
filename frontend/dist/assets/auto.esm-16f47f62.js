ec79b9757f673e7dc337ada4e734e1e6e53b4f30
andres-saa <andrew19f@gmail.com>
2024-10-04 20:40:14 -0500

    nueva carta

---
+++ frontend/dist/assets/auto.esm-16f47f62.js
@@ -0,0 +1,1 @@
+import{S as r,V as e}from"./index-2de7fe45.js";r.register(...e);export{r as default};
--- frontend/dist/assets/auto.esm-78d02528.js
+++
@@ -1,1 +0,0 @@
-import{S as r,V as e}from"./index-4deec0ce.js";r.register(...e);export{r as default};
