bfddc3fad3a1db3f56f8c095b99ada83f4b78b58
andres-saa <andrew19f@gmail.com>
2024-10-14 13:01:43 -0500

    nuevo sistema de datos

---
+++ frontend-delivery/dist/assets/pedidoManual-3ef62da9.js
@@ -0,0 +1,2 @@
+import{_ as r,o as t,c}from"./index-7174881e.js";const o={data(){return{iframeSrc:""}},mounted(){this.iframeSrc=`https://salchimonster.com/?nocache=${new Date().getTime()}`}},s=["src"];function a(n,i,m,f,e,_){return t(),c("iframe",{width:"100%",style:{height:"99vh"},src:e.iframeSrc,frameborder:"0"},`
+    `,8,s)}const h=r(o,[["render",a]]);export{h as default};
