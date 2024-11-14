c0a35eb2ba60d8d267da79a9e9b6d2a9ccae0134
andres-saa <andrew19f@gmail.com>
2024-11-14 17:09:28 -0500

    correccion de la s en ayuda

--- frontend/dist/assets/auto.esm-26f55e95.js
+++
@@ -1,1 +0,0 @@
-import{O as r,Q as e}from"./index-38375a5d.js";r.register(...e);export{r as default};
---
+++ frontend/dist/assets/auto.esm-f4170245.js
@@ -0,0 +1,1 @@
+import{O as r,Q as e}from"./index-a36e3104.js";r.register(...e);export{r as default};
