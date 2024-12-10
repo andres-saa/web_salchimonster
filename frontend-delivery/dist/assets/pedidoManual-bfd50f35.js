198c1cb831d8125165c573e82588c27c70ba3706
andres-saa <andrew19f@gmail.com>
2024-12-10 05:47:14 -0500

    nueva cocina

--- frontend-delivery/dist/assets/pedidoManual-bfd50f35.js
+++
@@ -1,2 +0,0 @@
-import{_ as r,o as t,c}from"./index-9a517b12.js";const o={data(){return{iframeSrc:""}},mounted(){this.iframeSrc=`https://salchimonster.com/?nocache=${new Date().getTime()}`}},s=["src"];function a(n,i,m,f,e,_){return t(),c("iframe",{width:"100%",style:{height:"99vh"},src:e.iframeSrc,frameborder:"0"},`
-    `,8,s)}const h=r(o,[["render",a]]);export{h as default};
