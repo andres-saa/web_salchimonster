d2d81907e071f57fe12a95796ddc1c2b75f80bc1
andres-saa <andrew19f@gmail.com>
2024-12-16 10:51:21 -0500

    correecion de salsas

---
+++ frontend/dist/assets/auto.esm-488a8580.js
@@ -0,0 +1,1 @@
+import{Q as r,R as e}from"./index-7ac1ab6e.js";r.register(...e);export{r as default};
--- frontend/dist/assets/auto.esm-52336f2e.js
+++
@@ -1,1 +0,0 @@
-import{Q as r,R as e}from"./index-d1a93b0f.js";r.register(...e);export{r as default};
