96f7182c9b3d59359cd223392fb093e3bf7d5493
andres-saa <andrew19f@gmail.com>
2025-01-05 19:56:27 -0600

    salchimonster new menu

---
+++ frontend/dist/assets/auto.esm-e23e898f.js
@@ -0,0 +1,1 @@
+import{Q as r,R as e}from"./index-c44b1756.js";r.register(...e);export{r as default};
--- frontend/dist/assets/auto.esm-ef89eb26.js
+++
@@ -1,1 +0,0 @@
-import{Q as r,R as e}from"./index-ec3c818c.js";r.register(...e);export{r as default};
