a51658b5dcea4e31c17bdebd814b7b9dd2f69ca6
andres-saa <andrew19f@gmail.com>
2025-01-05 20:09:50 -0600

    correccion menu

---
+++ frontend/dist/assets/auto.esm-6f2eb270.js
@@ -0,0 +1,1 @@
+import{Q as r,R as e}from"./index-e256d1b0.js";r.register(...e);export{r as default};
--- frontend/dist/assets/auto.esm-e23e898f.js
+++
@@ -1,1 +0,0 @@
-import{Q as r,R as e}from"./index-c44b1756.js";r.register(...e);export{r as default};
